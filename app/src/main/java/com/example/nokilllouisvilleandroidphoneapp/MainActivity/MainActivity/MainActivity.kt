01f83585ca00262d9cd286560600ca4c61d34682
Christianne1 <imaene@iu.edu>
2023-02-20 20:28:17 -0500

    original app

---
+++ app/src/main/java/com/example/nokilllouisvilleandroidphoneapp/MainActivity/MainActivity/MainActivity.kt
@@ -0,0 +1,116 @@
+package com.example.nokilllouisvilleandroidphoneapp.MainActivity.MainActivity
+
+import android.content.ContentValues.TAG
+import android.content.DialogInterface
+import android.content.Intent
+import android.os.Bundle
+import android.text.InputFilter
+import android.text.Spanned
+import android.util.Log
+import android.view.*
+import android.widget.EditText
+import android.widget.LinearLayout
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.nokilllouisvilleandroidphoneapp.MainActivity.MainActivity.LoginActivity
+import com.example.nokilllouisvilleandroidphoneapp.R
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter
+import com.firebase.ui.firestore.FirestoreRecyclerOptions
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
+
+data class User(
+    val displayName: String = "",
+    val emojis: String = ""
+)
+
+class UserViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+class MainActivity : AppCompatActivity() {
+
+    private val db = Firebase.firestore
+    private lateinit var auth: FirebaseAuth
+    private lateinit var rvUsers: RecyclerView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        auth = Firebase.auth
+        rvUsers = findViewById(R.id.rvUsers)
+
+        // Query the users collection
+        val query = db.collection("users")
+        val options = FirestoreRecyclerOptions.Builder<User>().setQuery(query, User::class.java)
+            .setLifecycleOwner(this).build()
+        val adapter = object : FirestoreRecyclerAdapter<User, UserViewHolder>(options) {
+            override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
+                val view = LayoutInflater.from(this@MainActivity)
+                    .inflate(android.R.layout.simple_list_item_2, parent, false)
+                return UserViewHolder(view)
+            }
+
+            override fun onBindViewHolder(holder: UserViewHolder, position: Int, model: User) {
+                val tvName: TextView = holder.itemView.findViewById(android.R.id.text1)
+                val tvEmojis: TextView = holder.itemView.findViewById(android.R.id.text2)
+                tvName.text = model.displayName
+                tvEmojis.text = model.emojis
+            }
+        }
+
+        rvUsers.adapter = adapter
+        rvUsers.layoutManager = LinearLayoutManager(this)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.miLogout) {
+            Log.i(TAG, "Logout")
+            auth.signOut()
+            val logoutIntent = Intent(this, LoginActivity::class.java)
+            logoutIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(logoutIntent)
+        } else if (item.itemId == R.id.miEdit) {
+            showAlertDialog()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun showAlertDialog() {
+
+        val editText = EditText(this)
+
+        val dialog = AlertDialog.Builder(this)
+            .setTitle("Update your parking spot")
+            .setView(editText)
+            .setNegativeButton("Cancel", null)
+            .setPositiveButton("OK", null)
+            .show()
+        dialog.getButton(DialogInterface.BUTTON_POSITIVE).setOnClickListener {
+            Log.i(TAG, "Clicked on positive button!")
+            val emojisEntered = editText.text.toString()
+            if (emojisEntered.isBlank()) {
+                Toast.makeText(this, "Cannot submit empty text", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            val currentUser = auth.currentUser
+            if (currentUser == null) {
+                Toast.makeText(this, "No signed in user", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            db.collection("users").document(currentUser.uid)
+                .update("emojis", emojisEntered)
+            dialog.dismiss()
+        }
+    }
+}
